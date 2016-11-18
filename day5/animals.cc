aaef86d236ad740266b0c4d6695372464d75821f
David Grellscheid <d.grellscheid+ictp13@gmail.com>
2016-11-18 11:53:22 +0100

    animals example

---
+++ day5/animals.cc
@@ -0,0 +1,204 @@
+#include <string>
+#include <iostream>
+
+namespace Zoo {
+
+class Animal {
+private:
+	unsigned int age_;
+	std::string name_;
+
+public:
+	Animal(std::string name) 
+		: age_(0), name_(name) 
+	{}// std::cout << "Animal constructor for " << name << "!\n";	}
+
+	Animal(const Animal & a) 
+		: age_(a.age_), name_(a.name_) 
+	{}// std::cout << "Animal copy constructor for " << a.name() << "!\n";	}
+
+
+
+
+	virtual std::string sound() const
+	{ 
+	 	return "..."; 
+	}
+
+
+
+
+	unsigned int age() const { return age_; }
+	std::string name() const { return name_; }
+
+	void birthday() { age_ += 1; }
+};
+
+
+
+
+
+
+class Dog : public Animal {
+private:
+	std::string favouriteToy_;
+public:
+	Dog(std::string name, std::string toy) 
+	    : Animal(name), favouriteToy_(toy) 
+	    {}// std::cout << "Dog constructor for " << name << "!\n";	}
+
+	std::string sound() const { 
+		return "<< bark >>"; 
+	}
+
+	std::string favouriteToy() const { return favouriteToy_; }
+};
+
+class Crocodile : public Animal {
+public:
+	Crocodile(std::string name) 
+	    : Animal(name)
+	    {}
+
+	// std::string sound() const { 
+	// 	return "come fa?"; 
+	// }
+};
+
+class NoisyCrocodile : public Crocodile {
+public:
+	NoisyCrocodile(std::string name) 
+	    : Crocodile(name)
+	    {}
+
+	std::string sound() const { 
+		return "<< roar >>"; 
+	}
+};
+
+
+
+class Cat : public Animal {
+private:
+	std::string favouritePlace_;
+public:
+	Cat(std::string name, std::string place) 
+	    : Animal(name), favouritePlace_(place) 
+	    {}// std::cout << "Cat constructor for " << name << "!\n";	}
+
+	std::string sound() const { 
+		return "<< miaow >>"; 
+	}
+
+	std::string favouritePlace() const { 
+		return favouritePlace_; 
+	}
+};
+
+
+bool isYounger(const Animal & a, const Animal & b) {
+	return a.age() < b.age();
+}
+
+
+bool sameSound(const Animal & a, const Animal & b) {
+ 	return a.sound() == b.sound();
+}
+
+
+
+}
+
+
+int main() {
+	// Zoo::Animal foo("Thing");
+	// std::cout << foo.name() << ' ' << foo.age() << '\n';
+ //    std::cout << "=========\n";
+
+	// Zoo::Crocodile bar("Pluto","Ball");
+	// std::cout << bar.name() << ' ' << bar.age() << '\n';
+	// bar.birthday();
+	// std::cout << bar.name() << ' ' << bar.age() << '\n';
+	// std::cout << bar.favouriteToy() << '\n';
+ //    std::cout << "=========\n";
+	
+	// Zoo::Cat baz("Tom","Sofa");
+	// std::cout << baz.name() << ' ' << baz.age() << '\n';
+	// baz.birthday();
+	// baz.birthday();
+	// baz.birthday();
+	// baz.birthday();
+	// std::cout << baz.name() << ' ' << baz.age() << '\n';
+	// std::cout << baz.favouritePlace() << '\n';
+ //    std::cout << "=========\n";
+	
+	// std::cout 
+	// 	<< std::boolalpha
+	// 	<< isYounger(bar, baz) << ' '
+	// 	<< isYounger(baz, bar)
+	// 	<< '\n';
+
+	//Zoo::Animal thingy("Thing"); // ?
+	Zoo::Dog    doggy ("Doggy","Bone");
+	Zoo::Cat    catty ("Catty","Sofa");
+	Zoo::NoisyCrocodile croc("Croc");
+
+	std::cout << "Original\n";
+    //std::cout << thingy.sound() << '\n';
+	std::cout <<  doggy.sound() << '\n';
+	std::cout <<  catty.sound() << '\n';
+	std::cout <<  croc.sound()  << '\n';
+
+	//Zoo::Animal & tng_ref = thingy;
+	Zoo::Animal & dog_ref = doggy;
+	Zoo::Animal & cat_ref = catty;
+	Zoo::Animal & croc_ref = croc;
+
+	std::cout << "Reference\n";
+    //std::cout << tng_ref.sound() << '\n';
+	std::cout << dog_ref.sound() << '\n';
+	std::cout << cat_ref.sound() << '\n';
+	std::cout << croc_ref.sound() << '\n';
+
+
+	// std::cout << dog_ref.sound() << '\n';
+	// std::cout << cat_ref.sound() << '\n';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// // calls Animal copy constructor
+	// Zoo::Animal dog_copy = doggy;
+	// Zoo::Animal cat_copy = catty;
+	// std::cout << dog_copy.sound() << '\n';
+	// std::cout << cat_copy.sound() << '\n';
+
+
+
+	return 0;
+}
+
+
+
+
+
+void printNameAndSound(const Zoo::Animal & a) {
+	std::cout 
+		<< a.name() << ' ' 
+		<< a.sound() << '\n';
+}
+
+
+
